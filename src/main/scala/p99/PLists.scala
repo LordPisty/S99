673687dd3d3451ee012222ef1eb6719ee9c73bd9
Stefano Crespi <stefano.crespi.85@gmail.com>
2014-09-25 11:42:55 -0700

    First problem plus tests.

---
+++ src/main/scala/p99/PLists.scala
@@ -0,0 +1,17 @@
+package p99
+
+/**
+ * Created by screspi on 9/25/14.
+ */
+object PLists {
+
+  // P01 (*) Find the last element of a list.
+  //     Example:
+  //     scala> last(List(1, 1, 2, 3, 5, 8))
+  //     res0: Int = 8
+  def last[A](ls: List[A]): A = {
+    if (ls.isEmpty) throw new NoSuchElementException
+    else if (ls.tail.isEmpty) ls.head else last(ls.tail)
+  }
+
+}
---
+++ src/test/scala/p99/PListsSuite.scala
@@ -0,0 +1,28 @@
+package p99
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+/**
+ * Created by screspi on 9/25/14.
+ */
+@RunWith(classOf[JUnitRunner])
+class PListsSuite extends FunSuite {
+  import PLists._
+
+  @Before def initialize() {
+  }
+
+  @Test def lastSuccess() {
+    assertTrue("The last element should be the last element", last(List(1, 1, 2, 3, 5, 8)) == 8)
+  }
+
+  test("Last on empty list should throw") {
+    intercept[NoSuchElementException] {last(List[Int]())}
+  }
+
+}
